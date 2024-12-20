51a73621f6e4802cbf023ac82b5ab1c1ae851a10
Piyush Srivastava <108412461+Socrii@users.noreply.github.com>
2024-12-20 23:11:02 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='py-6 border-t border-gray-200'>
       <p className='text-center text-lg text-white bg-black bg-opacity-50 px-4 py-2 rounded'>
-        Made with <span className='text-red-500'>❤️</span> from Piyush 
+        Made with <span className='text-red-500'>❤️</span>
       </p>
     </footer>
   );
